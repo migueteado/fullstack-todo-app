dc18d12fba57370188649d3d579d25ed861f8c49
migueteado <soymvillalobos@gmail.com>
2023-07-29 16:33:11 -0500

    ✨ Authentication with JWT

---
+++ src/lib/helpers/getEnvVariables.ts
@@ -0,0 +1,12 @@
+type EnvVariableKey = "JWT_SECRET_KEY" | "JWT_EXPIRES_IN";
+
+export function getEnvVariable(key: EnvVariableKey): string {
+  const value = process.env[key];
+
+  if (!value || value.length === 0) {
+    console.error(`The environment variable ${key} is not set.`);
+    throw new Error(`The environment variable ${key} is not set.`);
+  }
+
+  return value;
+}
