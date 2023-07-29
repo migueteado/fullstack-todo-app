399d11129651b9fa29979f6067bc394045e4f6f8
migueteado <soymvillalobos@gmail.com>
2023-07-29 16:01:03 -0500

    🗃️  Included createdAt and updatedAt

---
+++ prisma/migrations/20230729210015_dates/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `ToDoItem` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `ToDoList` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "ToDoItem" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "ToDoList" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
