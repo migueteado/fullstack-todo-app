b255768917127617261ed21d039e2dede6d127c6
migueteado <soymvillalobos@gmail.com>
2023-07-31 10:38:24 -0500

    🔥 Removed NextUI, installed chadcn-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
+import "./globals.css";
 import type { Metadata } from "next";
-import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,9 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
