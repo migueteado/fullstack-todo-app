d23f5bf6225a91347860ef06c0954a708a36bbda
migueteado <soymvillalobos@gmail.com>
2023-07-31 11:18:11 -0500

    Revert "🔥 Removed NextUI, installed chadcn-ui"
    This reverts commit b255768917127617261ed21d039e2dede6d127c6.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import Image from "next/image";
+import { Button } from "@nextui-org/react";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24"></main>
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <Button>Click me</Button>
+    </main>
   );
 }
