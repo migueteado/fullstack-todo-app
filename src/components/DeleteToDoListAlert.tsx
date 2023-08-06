fbfede108306f31e7e19199c4ed4a7628de88e69
Miguel Villalobos <27830744+migueteado@users.noreply.github.com>
2023-08-05 23:08:39 -0500

    ✨ Delete To Do List ready

---
+++ src/components/DeleteToDoListAlert.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useToast } from "./ui/use-toast";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Trash } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { ToDoList } from "@prisma/client";
+import { apiDeleteToDoList } from "@/lib/api";
+import { ErrorResponse, ToDoListDeleteResponse } from "@/lib/types";
+
+interface DeleteToDoListAlertProps {
+  id: ToDoList["id"];
+  handleDelete: (id: ToDoList["id"]) => void;
+}
+
+export default function DeleteToDoListAlert({
+  id,
+  handleDelete,
+}: DeleteToDoListAlertProps) {
+  const { toast } = useToast();
+  const [open, setOpen] = useState(false);
+
+  async function onContinue() {
+    const result = await apiDeleteToDoList({ data: { id: id } });
+
+    if (result.status === "success") {
+      const { deleteToDoList } = (result as ToDoListDeleteResponse).data;
+      toast({
+        title: "To Do List deleted!",
+        description: `To Do List with title ${
+          deleteToDoList.title.length > 70
+            ? deleteToDoList.title.slice(0, 70) + "..."
+            : deleteToDoList.title
+        } has been succesfully deleted.`,
+      });
+      handleDelete(deleteToDoList.id);
+      setOpen(false);
+    } else {
+      toast({
+        title: "An error occured!",
+        description: (result as ErrorResponse).message,
+      });
+    }
+  }
+
+  return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" size="icon">
+          <Trash className="w-4 h-4" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete this to
+            do list and remove its data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onContinue}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
