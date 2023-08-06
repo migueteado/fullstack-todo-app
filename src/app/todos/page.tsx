13dbf50ac1bc989871c5c2769558634601c07bf1
Miguel Villalobos <27830744+migueteado@users.noreply.github.com>
2023-08-05 23:07:50 -0500

    ⚡️ Now using server components

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,38 +1,14 @@
-"use client";
-
 import { AlertMessage } from "@/components/AlertDestructive";
-import { CreateToDoListDialog } from "@/components/CreateToDoListDialog";
 import { Header } from "@/components/Header";
+import ToDoLists from "@/components/ToDoListList";
 import { apiGetToDoLists } from "@/lib/api";
 import { ErrorResponse, ToDoListManyResponse } from "@/lib/types";
-import useStore from "@/store";
-import { ToDoList } from "@prisma/client";
-import { useEffect, useState } from "react";
+import { cookies } from "next/headers";
 
-export default function Todos() {
-  const store = useStore();
-  const [todos, setTodos] = useState<ToDoList[]>([]);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchTodos = async () => {
-      store.setRequestLoading(true);
-      const res = await apiGetToDoLists({ token: store.token || "" });
-
-      if (res.status === "success") {
-        setTodos((res as ToDoListManyResponse).data.toDoLists);
-      } else {
-        setErrorMessage((res as ErrorResponse).message);
-      }
-      store.setRequestLoading(false);
-    };
-
-    fetchTodos();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function createToDoList() {}
+export default async function Todos() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+  const res = await apiGetToDoLists({ token: token });
 
   return (
     <>
@@ -40,19 +16,17 @@ export default function Todos() {
       <main className="container">
         <div className="w-full max-w-4xl">
           <h1 className="page-title">ToDo Lists</h1>
-          <div className="flex justify-end py-2 mb-4 border-b border-slate-200">
-            <CreateToDoListDialog />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            {errorMessage && (
-              <AlertMessage type="error" title="Error" message={errorMessage} />
-            )}
-            {todos && todos.length > 0 ? (
-              <></>
-            ) : (
-              <p>Start by creating your first to do list.</p>
-            )}
-          </div>
+          {res.status === "success" ? (
+            <ToDoLists
+              toDoLists={(res as ToDoListManyResponse).data.toDoLists}
+            />
+          ) : (
+            <AlertMessage
+              type="error"
+              title="Error getting ToDo Lists"
+              message={(res as ErrorResponse).message}
+            />
+          )}
         </div>
       </main>
     </>
